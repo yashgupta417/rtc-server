d66808232ca98408201c2369b4fd067f5e297f3b
Yash <yashgupta417@gmail.com>
2021-02-19 14:17:04 +0530

    setting up project structure

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express=require("express")
 const app=express()
 
-
+app.get("/",function(req,res){
+    res.send("RTC")
+})
 
 app.listen(3000,function(){
     console.log("Server is running...")
