b0a954f99a5afb1f20f5e0a6491c723ac8f0d063
Yash <yashgupta417@gmail.com>
2021-03-03 14:16:56 +0530

    updated agora route

--- routes/agora.js
+++ routes/agora.js
@@ -1,6 +1,8 @@
 const express=require('express')
 const router=express.Router()
 const {RtcTokenBuilder, RtmTokenBuilder, RtcRole, RtmRole} = require('agora-access-token')
+const User=require("../models/user")
+
 
 const appID=process.env.AGORA_APP_ID
 const appCertificate=process.env.AGORA_APP_CERTIFICATE
@@ -11,14 +13,19 @@ const expirationTimeInSeconds=3600*24 + Math.floor(Date.now()/1000)
 
 
 
-router.get('/agora/token',function(req,res){
+router.get('/agora/token',async function(req,res){
     let {roomName, username}=req.query
 
     if(!roomName || !username) return res.status(400).send("roomName and username required")
 
-    const token=RtcTokenBuilder.buildTokenWithAccount(appID, appCertificate, roomName, username, role, expirationTimeInSeconds)
+    const user=await User.findOne({username:username})
+                            .select('-_id name username email createdAt roomsCount')
+                            .exec()
 
-    res.send({token: token})
+    const userString=JSON.stringify(user)
+    const token=RtcTokenBuilder.buildTokenWithAccount(appID, appCertificate, roomName, userString, role, expirationTimeInSeconds)
+
+    res.send({token: token, userString: userString})
 })
 
 module.exports=router
