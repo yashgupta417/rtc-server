45b999665105804827513cd375033c3fcbccec05
Yash <yashgupta417@gmail.com>
2021-04-05 17:07:53 +0530

    join room socket updated

--- eventHandlers/chat.js
+++ eventHandlers/chat.js
@@ -2,8 +2,37 @@ const Room=require("../models/room")
 const Message=require("../models/message")
 const User=require("../models/user")
 
+
+async function getRoom(address) {
+    const room=await Room.findOne({address:address})
+                            .select('-_id name address image members owner createdAt')
+                            .populate('members','-_id name username image email createdAt roomsCount')
+                            .populate('owner','-_id name username image email createdAt roomsCount')
+                            .exec()
+
+    return room
+}
+
+async function generateRoomToken(username,roomName) {
+    const user=await User.findOne({username:username})
+                            .select('-_id name username image')
+                            .exec()
+
+    let userString=JSON.stringify(user)
+
+    //replacing " since they are not allowed in agora, replace them back on client side
+    userString=userString.replace(/\"/g,"#")
+
+    //replacing '/'
+    userString=userString.replace(/\//g,";")
+
+    const token=RtcTokenBuilder.buildTokenWithAccount(appID, appCertificate, roomName, userString, role, expirationTimeInSeconds)
+
+    return userString,token
+}
+
 module.exports=(io)=>{
-    const joinRoom=async function(address,cb){
+    const joinRoom=async function(address,username,cb){
         const socket=this
 
         //join room
@@ -11,13 +40,16 @@ module.exports=(io)=>{
         console.log(socket.rooms)
 
         //get all messages
-        const room=await Room.findOne({address:address}).exec()
+        const room=await getRoom(address)
         const messages=await Message.find({to:room._id})
                                 .populate('sender','-_id name username image email createdAt roomsCount')
                                 .exec()
 
+        //generate room token
+        userString,token=await generateRoomToken(username,room.name)
+
         //trigger callback
-        cb(messages)
+        cb(messages,room,token,userString)
     }
 
     const sendMessage=async function(text, username, address,cb){
