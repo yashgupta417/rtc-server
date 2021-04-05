d77a7dd780c726e6a01c9f7ea96bacd4e400070e
Yash <yashgupta417@gmail.com>
2021-04-05 17:25:53 +0530

    fixing error

--- eventHandlers/chat.js
+++ eventHandlers/chat.js
@@ -46,7 +46,7 @@ module.exports=(io)=>{
                                 .exec()
 
         //generate room token
-        userString,token=await generateRoomToken(username,room.name)
+        const userString,token=await generateRoomToken(username,room.name)
 
         //trigger callback
         cb(messages,room,token,userString)
